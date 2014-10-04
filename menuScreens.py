203ef19edf9a2994c881bb22b5914c0df93c15dd
Matthew Robinson <omegachysis@outlook.com>
2014-10-04 10:37:25 -0400

    Created menu quit button

--- menuScreens.py
+++ menuScreens.py
@@ -12,7 +12,7 @@ class MainScreen(arche.Application):
         self._createInterface()
 
     def _createInterface(self):
-        self.buttonQuit = arche.interface.SolidButton(
+        self.buttonQuitX = arche.interface.SolidButton(
             width = 40, height = 25,
             colorReset = (100,100,100),
             colorHover = (255,50,50),
@@ -25,7 +25,26 @@ class MainScreen(arche.Application):
                 size = 25,
                 font = "font/consola.ttf"),
             )
-        self.buttonQuit.right = self.game.width + 1
-        self.buttonQuit.top = -1
+        self.buttonQuitX.right = self.game.width + 1
+        self.buttonQuitX.top = -1
+        self.buttonQuitX.name = "x button"
+        self.addSprite(self.buttonQuitX)
+
+        self.buttonQuit = arche.interface.SolidButton(
+            width = self.xprop(.2),
+            height= self.yprop(.1),
+            colorReset = (80,80,255),
+            colorHover = (120,120,255),
+            colorPress = (220,220,255),
+            command = self.game.quit,
+            textObject = arche.Text(
+                value = "Quit",
+                x = 0, y = 0,
+                color = (255,255,255),
+                size = self.xprop(.04),
+                font = "font/consola.ttf"),
+            )
+        self.buttonQuit.x = self.xprop(.75)
+        self.buttonQuit.y = self.yprop(.85)
         self.buttonQuit.name = "quit button"
         self.addSprite(self.buttonQuit)
