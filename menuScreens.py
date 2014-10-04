9194626384cacf79dc59fa72c3779dd4464dd15c
Matthew Robinson <omegachysis@outlook.com>
2014-10-04 10:15:53 -0400

    Create quit button

--- menuScreens.py
+++ menuScreens.py
@@ -8,3 +8,24 @@ class MainScreen(arche.Application):
         super().__init__()
 
         self.backgroundColor = (200,200,255)
+
+        self._createInterface()
+
+    def _createInterface(self):
+        self.buttonQuit = arche.interface.SolidButton(
+            width = 40, height = 25,
+            colorReset = (100,100,100),
+            colorHover = (255,50,50),
+            colorPress = (255,150,150),
+            command = self.game.quit,
+            textObject = arche.Text(
+                value = "X",
+                x = 0, y = 0,
+                color = (255,255,255),
+                size = 25,
+                font = "font/consola.ttf"),
+            )
+        self.buttonQuit.right = self.game.width + 1
+        self.buttonQuit.top = -1
+        self.buttonQuit.name = "quit button"
+        self.addSprite(self.buttonQuit)
